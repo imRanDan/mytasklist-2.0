07484e3b72a97dfa59cfd2f9025699e8a46f6063
Danyal Imran <dan.imran97@gmail.com>
2023-11-26 14:47:29 -0500

    added in more info to features section

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -11,8 +11,8 @@ const Features = () => {
 		</div>
 		<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
 			<div>
-				<h3 className="text-2xl font-bold tracki sm:text-3xl text-gray-900">Ad vix debet docendi</h3>
-				<p className="mt-3 text-lg text-gray-600">Ne dicta praesent ocurreret has, diam theophrastus at pro. Eos etiam regione ut, persius eripuit quo id. Sit te euismod tacimates.</p>
+				<h3 className="text-2xl font-bold tracki sm:text-3xl text-gray-900">Get into a state of flow for effective task management!</h3>
+				<p className="mt-3 text-lg text-gray-600">myTaskList will help you get focused with tips and pointers to easily get into a state of flow.</p>
 				<div className="mt-12 space-y-12">
 					<div className="flex">
 						<div className="flex-shrink-0">
@@ -23,8 +23,8 @@ const Features = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leadi text-gray-900">Per ei quaeque sensibus</h4>
-							<p className="mt-2 text-gray-600">Ex usu illum iudico molestie. Pro ne agam facete mediocritatem, ridens labore facete mea ei. Pro id apeirian dignissim.</p>
+							<h4 className="text-lg font-medium leadi text-gray-900">Create, Update, and Delete your tasks!</h4>
+							<p className="mt-2 text-gray-600">Not just a typical CRUD app! Create and manage your tasks with style!</p>
 						</div>
 					</div>
 					<div className="flex">
@@ -36,8 +36,8 @@ const Features = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leadi text-gray-900">Cu imperdiet posidonium sed</h4>
-							<p className="mt-2 text-gray-600">Amet utinam aliquando ut mea, malis admodum ocurreret nec et, elit tibique cu nec. Nec ex maluisset inciderint, ex quis.</p>
+							<h4 className="text-lg font-medium leadi text-gray-900">Set your own pomodoro timer!</h4>
+							<p className="mt-2 text-gray-600">For every task, you can set your own pomodoro timer to take as much time (and breaks) needed for successful task management</p>
 						</div>
 					</div>
 					<div className="flex">
@@ -49,66 +49,14 @@ const Features = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leadi text-gray-900">Nulla omittam sadipscing mel ne</h4>
-							<p className="mt-2 text-gray-600">At sed possim oporteat probatus, justo graece ne nec, minim commodo legimus ut vix. Ut eos iudico quando soleat, nam modus.</p>
+							<h4 className="text-lg font-medium leadi text-gray-900">Motivational quotes and clean dashboard</h4>
+							<p className="mt-2 text-gray-600">Our app has a clean dashboard for your organizational needs and motivational quotes that will randomize each time you refresh the page.</p>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div aria-hidden="true" className="mt-10 lg:mt-0">
 				<img src="https://source.unsplash.com/random/360x480" alt="" className="mx-auto rounded-lg shadow-lg bg-gray-500" />
-			</div>
-		</div>
-		<div>
-			<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
-				<div className="lg:col-start-2">
-					<h3 className="text-2xl font-bold tracki sm:text-3xl text-gray-900">Eam nibh gloriatur ex</h3>
-					<p className="mt-3 text-lg text-gray-600">Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.</p>
-					<div className="mt-12 space-y-12">
-						<div className="flex">
-							<div className="flex-shrink-0">
-								<div className="flex items-center justify-center w-12 h-12 rounded-md bg-blue-600 text-gray-50">
-									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
-									</svg>
-								</div>
-							</div>
-							<div className="ml-4">
-								<h4 className="text-lg font-medium leadi text-gray-900">Cibo augue offendit has ad</h4>
-								<p className="mt-2 text-gray-600">An per velit appellantur, ut utinam minimum nominavi sit, odio nostro habemus ne nec. Ne sonet regione contentiones est.</p>
-							</div>
-						</div>
-						<div className="flex">
-							<div className="flex-shrink-0">
-								<div className="flex items-center justify-center w-12 h-12 rounded-md bg-blue-600 text-gray-50">
-									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
-									</svg>
-								</div>
-							</div>
-							<div className="ml-4">
-								<h4 className="text-lg font-medium leadi text-gray-900">At eum ferri luptatum lobortis</h4>
-								<p className="mt-2 text-gray-600">Te per quidam maiorum ocurreret, etiam delicatissimi usu ad. Ne has quod periculis. Te sit primis iisque efficiantur.</p>
-							</div>
-						</div>
-						<div className="flex">
-							<div className="flex-shrink-0">
-								<div className="flex items-center justify-center w-12 h-12 rounded-md bg-blue-600 text-gray-50">
-									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
-									</svg>
-								</div>
-							</div>
-							<div className="ml-4">
-								<h4 className="text-lg font-medium leadi text-gray-900">Dicunt verterem evertitur eu sea</h4>
-								<p className="mt-2 text-gray-600">Audire principes rationibus eam an, autem nominavi luptatum per te. Sumo fabulas vim eu, sonet saperet eleifend ut vix.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="mt-10 lg:mt-0 lg:col-start-1 lg:row-start-1">
-					<img src="https://source.unsplash.com/random/361x481" alt="" className="mx-auto rounded-lg shadow-lg bg-gray-500" />
-				</div>
 			</div>
 		</div>
 	</div>
