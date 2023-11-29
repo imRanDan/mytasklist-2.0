15f89ea886dd3cdbdc6b02e81649d24cf356af96
Danyal Imran <dan.imran97@gmail.com>
2023-11-28 20:20:41 -0500

    removed a state and function to see if app will launch;

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,8 @@
-"use client"
-
-import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import SideBar from './SideBar'
 
 const Nav = () => {
-	//  state to manage sidebar open/close
-	const [isOpen, setIsOpen] = useState(false);
-
-	// toggle function for navbar state
-	const toggleSideBar = () => {
-		console.log("before toggle:", isOpen)
-		setIsOpen(!isOpen)
-		console.log("after toggle:", isOpen)
-	}
 
   return (
 <header className="p-4 bg-gray-100 text-gray-800">
@@ -46,12 +34,6 @@ const Nav = () => {
 			<button className="self-center px-8 py-3 rounded">Sign in</button>
 			<button className="self-center px-8 py-3 font-semibold rounded bg-blue-600 text-gray-50">Sign up</button>
 		</div>
-		<button className="p-4 lg:hidden" onClick={() => console.log('Button clicked')}>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-gray-800">
-				<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-			</svg>
-		</button>
-		{isOpen && <SideBar />}
 	</div>
 </header>
   )
