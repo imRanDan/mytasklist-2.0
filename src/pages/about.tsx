90425cff9fa37318354027cae8d769c818dabe82
Danyal Imran <dan.imran97@gmail.com>
2023-12-01 19:24:42 -0500

    working on /about ui/ux. Running into hydration issues.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,13 @@
 import Nav from '@/components/Nav'
 import React from 'react'
 import Image from 'next/image'
+import MoreInfo from '@/components/MoreInfo'
 
 const about = () => {
   return (
     <>
       <Nav />
+      <MoreInfo/>
     </>
   )
 }
