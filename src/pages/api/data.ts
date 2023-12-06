846b9793183b8b0fcaae89cc7df1775e4b49c492
Danyal Imran <dan.imran97@gmail.com>
2023-12-06 14:45:54 -0500

    added in data.ts for db operations and error handling;

---
+++ src/pages/api/data.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "@/lib/db";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { db } = await connectToDatabase();
+    // Database operations are performed here
+    const data = await db.collection('mytasklist').find({}).toArray()
+    res.status(200).json(data)
+  } catch (error) {
+    // Error handling here
+    res.status(500).json({ error: 'Failed to connect to db BIG OOF!'})
+  }
+}
