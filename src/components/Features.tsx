d79c295865ce9419c470ff9579279ca573d03358
Danyal Imran <dan.imran97@gmail.com>
2024-01-13 22:03:38 -0500

    working on darkmode for all components

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Features = () => {
   return (
     <>
-<section className="bg-gray-100 text-gray-800">
+<section className="bg-gray-100 text-gray-800 dark:bg-gray-900">
 	<div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
 		<div>
-			<h2 className="text-3xl font-bold tracki text-center sm:text-5xl text-gray-900">Features</h2>
-			<p className="max-w-3xl mx-auto mt-4 text-xl text-center text-gray-600">MyTaskList has more features than before, offering a system for your task management.</p>
+			<h2 className="text-3xl font-bold tracki text-center sm:text-5xl text-gray-900 dark:text-white">Features</h2>
+			<p className="max-w-3xl mx-auto mt-4 text-xl text-center text-gray-600 dark:text-white">MyTaskList has more features than before, offering a system for your task management.</p>
 		</div>
-		<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+		<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center ">
 			<div>
-				<h3 className="text-2xl font-bold tracki sm:text-3xl text-gray-900">Get into a state of flow for effective task management!</h3>
-				<p className="mt-3 text-lg text-gray-600">myTaskList will help you get focused with tips and pointers to easily get into a state of flow.</p>
+				<h3 className="text-2xl font-bold tracki sm:text-3xl text-gray-900 dark:text-white">Get into a state of flow for effective task management!</h3>
+				<p className="mt-3 text-lg text-gray-600 dark:text-white">myTaskList will help you get focused with tips and pointers to easily get into a state of flow.</p>
 				<div className="mt-12 space-y-12">
 					<div className="flex">
 						<div className="flex-shrink-0">
@@ -23,8 +23,8 @@ const Features = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leadi text-gray-900">Create, Update, and Delete your tasks!</h4>
-							<p className="mt-2 text-gray-600">Not just a typical CRUD app! Create and manage your tasks with style!</p>
+							<h4 className="text-lg font-medium leadi text-gray-900 dark:text-white">Create, Update, and Delete your tasks!</h4>
+							<p className="mt-2 text-gray-600 dark:text-white">Not just a typical CRUD app! Create and manage your tasks with style!</p>
 						</div>
 					</div>
 					<div className="flex">
@@ -36,8 +36,8 @@ const Features = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leadi text-gray-900">Set your own pomodoro timer!</h4>
-							<p className="mt-2 text-gray-600">For every task, you can set your own pomodoro timer to take as much time (and breaks) needed for successful task management</p>
+							<h4 className="text-lg font-medium leadi text-gray-900 dark:text-white">Set your own pomodoro timer!</h4>
+							<p className="mt-2 text-gray-600 dark:text-white">For every task, you can set your own pomodoro timer to take as much time (and breaks) needed for successful task management</p>
 						</div>
 					</div>
 					<div className="flex">
@@ -49,8 +49,8 @@ const Features = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leadi text-gray-900">Motivational quotes and clean dashboard</h4>
-							<p className="mt-2 text-gray-600">Our app has a clean dashboard for your organizational needs and motivational quotes that will randomize each time you refresh the page.</p>
+							<h4 className="text-lg font-medium leadi text-gray-900 dark:text-white">Motivational quotes and clean dashboard</h4>
+							<p className="mt-2 text-gray-600 dark:text-white">Our app has a clean dashboard for your organizational needs and motivational quotes that will randomize each time you refresh the page.</p>
 						</div>
 					</div>
 				</div>
--- src/components/MoreInfo.tsx
+++ src/components/MoreInfo.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const MoreInfo = () => {
   return (
     <>
-    <section className="m-4 md:m-8 bg-gray-100 text-gray-800">
-	<div className="container mx-auto p-4 my-6 space-y-2 text-center">
-		<h2 className="text-5xl font-bold">Empowering Your Productivity Journey</h2>
-		<p className="text-gray-600">At mytasklist, we believe in simplifying the way you manage tasks and track progress.</p>
-	</div>
+    <section className="bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-white">
+			<div className="container mx-auto p-4 pt-8 space-y-2 text-center">
+			<h2 className="text-5xl font-bold">Empowering Your Productivity Journey</h2>
+			<p className="text-gray-600 dark:text-white">At mytasklist, we believe in simplifying the way you manage tasks and track progress.</p>
+			</div>
 
-	<div className="container mx-auto grid justify-center text-center gap-5 sm:grid-cols-2 lg:grid-cols-3">
+	<div className="container mx-auto grid justify-center text-center p-6 gap-5 sm:grid-cols-2 lg:grid-cols-3 dark:bg-gray-900">
 		<div className="flex flex-col items-center p-4">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 text-violet-600">
 				<path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd"></path>
