f8d5e637b058a887114a7cafb8adfb55f7564e06
Danyal Imran <dan.imran97@gmail.com>
2024-01-14 20:34:52 -0500

    column button working onclick

--- src/components/Kanban.tsx
+++ src/components/Kanban.tsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import PlusIcon from '@/icons/PlusIcon'
+import {useState} from "react"
+import { Column } from '@/types'
 
 function Kanban() {
+  const [columns, setColumns] = useState<Column[]>([])
+  console.log(columns)
   return (
     <div className='m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]'>
       <div className='m-auto'>
-        <button className='h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-grey-800 border-2 border-grey-900 p-4 ring-green-500 hover:ring-2 flex gap-2'>
+        <button onClick={() => {
+          createNewColumn();
+        }}
+        className='h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-grey-800 border-2 border-grey-900 p-4 ring-green-500 hover:ring-2 flex gap-2'>
           <PlusIcon/> Add Column
         </button>
       </div>
     </div>
   )
+
+  function createNewColumn() {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1},`
+    }
+
+    setColumns([...columns, columnToAdd])
+  }
+}
+
+function generateId() {
+  // Generate a random number btwn 0 and 1000
+  return Math.floor(Math.random() * 1001)
 }
 
 export default Kanban
